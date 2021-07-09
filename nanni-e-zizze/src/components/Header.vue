8728280b56e2ec9fe9cc12110838bccaa7115be0
docssand <alessandro.pintus94@gmail.com>
2021-07-09 09:04:39 +0200

    init(main): creazione progetto

---
+++ nanni-e-zizze/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+      <picture>
+        <source srcset="../assets/logo-mobile.png"
+            media="(min-width: 800px)">
+        <img class="header__logo px-4" src="../assets/logo-mobile.png" alt="" />
+      </picture>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss">
+    .header {
+        height: 4em;
+        &__logo{
+            display: flex;
+            justify-self: left;
+            height: 3rem;
+        }
+    }
+</style>
