e4a0853ce61280c3a0db2ecfac71a1ece021f63f
Alessandro <alessandro.pintus94@gmail.com>
2021-09-29 21:02:11 +0200

    chore: update deps

--- src/components/PhoneCTA.vue
+++ src/components/PhoneCTA.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="
-            phone-cta 
-            d-flex justify-content-center align-items-center
-            bg-primary">
-      <font-awesome-icon 
-        class="phone-cta__ico text-white" 
-        icon="phone" 
-        href="tel:3333333333"
-        />
+  <div
+    class="
+      phone-cta
+      d-flex
+      justify-content-center
+      align-items-center
+      bg-primary
+    "
+  >
+    <font-awesome-icon
+      class="phone-cta__ico text-white"
+      icon="phone"
+      href="tel:3333333333"
+    />
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang=scss>
-    .phone-cta {
-        position: fixed;
-        bottom: 10px;
-        right: 10px;
-        border: 2px solid $light;
-        border-radius: 50%;
-        height: 4rem;
-        width: 4rem;
-        &__ico{
-            font-size: 2em;
-        }
-    }
+.phone-cta {
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  border: 2px solid $light;
+  border-radius: 50%;
+  height: 4rem;
+  width: 4rem;
+  &__ico {
+    font-size: 2em;
+  }
+}
 </style>
